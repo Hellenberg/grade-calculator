fc62d82f2906e734c367ea2d54011b241dffc59a
Rickard Hellenberg <riche828@student.liu.se>
2018-11-09 16:03:50 +0100

    Updates

---
+++ precache-manifest.66c044802434907a0e44b1c7cc586b89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "31a3077a8f691ddee1b5",
+    "url": "/grade-calculator/static/js/runtime~main.31a3077a.js"
+  },
+  {
+    "revision": "822af4b49da002e37ef0",
+    "url": "/grade-calculator/static/js/main.822af4b4.chunk.js"
+  },
+  {
+    "revision": "ad4591f84e9b761fa829",
+    "url": "/grade-calculator/static/js/1.ad4591f8.chunk.js"
+  },
+  {
+    "revision": "822af4b49da002e37ef0",
+    "url": "/grade-calculator/static/css/main.38618b38.chunk.css"
+  },
+  {
+    "revision": "6586be957a683dbce27e612f2b0988aa",
+    "url": "/grade-calculator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/grade-calculator/precache-manifest.1b19621c8c83e39cf96e57058aec1bb0.js"
+  "/grade-calculator/precache-manifest.66c044802434907a0e44b1c7cc586b89.js"
 );
 
 workbox.clientsClaim();
